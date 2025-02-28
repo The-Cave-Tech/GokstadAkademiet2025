b5317f435a7a267d23ba499e8916888e9bf84d00
Simen Skogen <sim1skogen@gmail.com>
2025-02-28 14:46:27 +0100

    Added base structure for dashboard. Fixed structure problems

---
+++ frontend/src/app/(PublicPages)/(home)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+/*test data oppsto problemer med db, så fikk ikke lagt til noe, og ble mer feilsøking med db*/
+
+export default function dashboard() {
+  return (
+    <section>
+        <div>
+
+            <h1>Dummydata</h1>
+        </div>
+        <div>
+            <button>Hello</button>
+            <button>Hello</button>
+            <button>Hello</button>
+            <button>Hello</button>
+        </div>
+    </section>
+  )
+}
