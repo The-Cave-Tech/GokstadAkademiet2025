82e3b95eee191e98bbb3e8caa1412aa4a4d67259
Bendik Bror Schøyen Grue <bendikbror@gmail.com>
2025-03-03 14:33:48 +0100

    lagt til header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
+import { Header } from "@/components/ui/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +36,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><Header />
         {children}
       </body>
     </html>
