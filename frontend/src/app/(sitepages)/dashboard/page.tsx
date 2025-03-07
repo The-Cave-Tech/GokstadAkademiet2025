be5c8b7b6defaf2e34625b2aa45fc1b7bdc123f8
Simen Skogen <sim1skogen@gmail.com>
2025-03-07 10:06:42 +0100

    Structure, formatting and styling changes

--- frontend/src/app/(sitepages)/dashboard/page.tsx
+++ frontend/src/app/(sitepages)/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { UserProfile } from "@/types/dashboard";
-import { fetchUserProfile } from "@/lib/services/strapiProfileData";
+import { fetchUserProfile } from "@/lib/data/services/strapiProfileData";
 import AuthCard from "@/components/ui/AuthCard";
 import Link from "next/link";
 
@@ -24,7 +24,7 @@ export default function Dashboard() {
   if (!profile) return <p>Loading.....</p>;
 
   return (
-    <section className="flex flex-col justify-center items-center gap-5">
+    <section className="grid gap-5 my-10">
       <section className="max-w-4xl mx-auto px-4 py-8">
         <div>
           {/* Background Image */}
@@ -40,130 +40,181 @@ export default function Dashboard() {
           <p className="text-center">{profile.bio}</p>
         </div>
       </section>
-      <div className="relative flex py-5 items-center w-[100%]">
+      <div className="flex items-center">
         <div className="flex-grow border-t-4 border-green-400"></div>
         <span className="flex-shrink mx-4 text-red-900">Adminstration</span>
         <div className="flex-grow border-t-4 border-green-400"></div>
       </div>
-      <section className="w-10/12 h-10/10 flex flex-wrap justify-center gap-8">
-        <Link href="/dashboard/user/personalInfo">
-          <AuthCard
-            className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
-            header={<h1>My personal info</h1>}
-            content={
-              <section className="flex gap-5">
-                <section className="flex flex-col gap-5">
-                  <p>We are gonna have a blast</p>
+      <section className="mx-auto p-4" aria-label="Administer your page">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
                 </section>
-              </section>
-            }
-            footer
-          />
-        </Link>
-        <AuthCard
-          className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
-          header={<h1>My payments</h1>}
-          content={
-            <section className="flex gap-5">
-              <section className="flex flex-col gap-5">
-                <p>We are gonna have a blast</p>
-                <p>View your payment historic</p>
-                <p>View your wishlist</p>
-                <p>View your payment ways</p>
-              </section>
-            </section>
-          }
-          footer
-        />
-        <AuthCard
-          className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
-          header={<h1>My events</h1>}
-          content={
-            <section className="flex gap-5">
-              <section className="flex flex-col gap-5">
-                <p>We are gonna have a blast</p>
-                <p>View your active events</p>
-                <p>View your previous events</p>
-              </section>
-            </section>
-          }
-          footer
-        />
-        <AuthCard
-          className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
-          header={<h1>This is card link</h1>}
-          content={
-            <section className="flex gap-5">
-              <section className="flex flex-col gap-5">
-                <p>We are gonna have a blast</p>
-              </section>
-            </section>
-          }
-          footer
-        />
-        <AuthCard
-          className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
-          header={<h1>This is card link</h1>}
-          content={
-            <section className="flex gap-5">
-              <section className="flex flex-col gap-5">
-                <p>We are gonna have a blast</p>
-              </section>
-            </section>
-          }
-          footer
-        />
+              }
+              footer
+            />
+          </Link>
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
+                </section>
+              }
+              footer
+            />
+          </Link>
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
+                </section>
+              }
+              footer
+            />
+          </Link>
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
+                </section>
+              }
+              footer
+            />
+          </Link>
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
+                </section>
+              }
+              footer
+            />
+          </Link>
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
+                </section>
+              }
+              footer
+            />
+          </Link>
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
+                </section>
+              }
+              footer
+            />
+          </Link>
+          <Link href="/dashboard/user/personalInfo">
+            <AuthCard
+              className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-60"
+              header={<h1>My personal info</h1>}
+              content={
+                <section className="flex gap-5">
+                  <section className="flex flex-col gap-5">
+                    <p>We are gonna have a blast</p>
+                  </section>
+                </section>
+              }
+              footer
+            />
+          </Link>
+        </div>
       </section>
       {/* Section only for admin role */}
       {role === "admin" && (
-        <section className="flex flex-wrap justify-center items-center">
+        <section aria-label="Administer system">
           <div className="flex py-5 items-center w-[100vw]">
             <div className="flex-grow border-t-4 border-green-400"></div>
-            <span className="flex-shrink mx-4 text-red-900">Only for admin</span>
+            <span className="flex-shrink mx-4 text-red-900">
+              Only for admin
+            </span>
             <div className="flex-grow border-t-4 border-green-400"></div>
           </div>
-          <div className="w-10/12 h-10/10 flex flex-wrap justify-center gap-8">
-            <Link href="/dashboard/user/personalInfo">
-              <AuthCard
-                className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
-                header={<h1>Administrate users</h1>}
-                content={
-                  <section className="flex gap-5">
+          <article className="mx-auto p-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+              <Link href="/dashboard/admin/personalInfo">
+                <AuthCard
+                  className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
+                  header={<h1>Administrate users</h1>}
+                  content={
+                    <section className="flex gap-5">
+                      <section className="flex flex-col gap-5">
+                        <p>We are gonna have a blast</p>
+                        <p>Change memberships</p>
+                      </section>
+                    </section>
+                  }
+                  footer
+                />
+              </Link>
+              <Link href="/dashboard/admin/personalInfo">
+                <AuthCard
+                  className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
+                  header={<h1>Bought items</h1>}
+                  content={
                     <section className="flex flex-col gap-5">
                       <p>We are gonna have a blast</p>
-                      <p>Change memberships</p>
+                      <p>Look at items bought</p>
                     </section>
-                  </section>
-                }
-                footer
-              />
-            </Link>
-            <Link href="/dashboard/user/personalInfo">
-              <AuthCard
-                className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
-                header={<h1>Bought items</h1>}
-                content={
-                  <section className="flex flex-col gap-5">
-                    <p>We are gonna have a blast</p>
-                    <p>Look at items bought</p>
-                  </section>
-                }
-                footer
-              />
-            </Link>
-            <Link href="/dashboard/user/personalInfo">
-              <AuthCard
-                className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
-                header={<h1>Change events</h1>}
-                content={
-                  <section className="flex flex-col gap-5">
-                    <p>We are gonna have a blast</p>
-                  </section>
-                }
-                footer
-              />
-            </Link>
-          </div>
+                  }
+                  footer
+                />
+              </Link>
+              <Link href="/dashboard/admin/personalInfo">
+                <AuthCard
+                  className="flex flex-col justify-center items-center bg-cyan-200 shadow-lg shadow-cyan-500/50 size-70"
+                  header={<h1>Change events</h1>}
+                  content={
+                    <section className="flex flex-col gap-5">
+                      <p>We are gonna have a blast</p>
+                    </section>
+                  }
+                  footer
+                />
+              </Link>
+            </div>
+          </article>
         </section>
       )}
     </section>
