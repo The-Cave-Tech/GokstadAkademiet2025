355fcf71d2f7484dcc335226ec38ee1227df7246
Aslan Khatuev <aslan.khatuev@outlook.com>
2025-04-05 16:16:43 +0200

    .

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,20 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface LandingPageHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_landing_page_hero_sections';
+  info: {
+    description: '';
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    landingImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    Subtitle: Schema.Attribute.String;
+    Title: Schema.Attribute.String;
+  };
+}
+
 export interface UserInformationUserInformation extends Struct.ComponentSchema {
   collectionName: 'components_user_information_user_informations';
   info: {
@@ -11,6 +26,7 @@ export interface UserInformationUserInformation extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'landing-page.hero-section': LandingPageHeroSection;
       'user-information.user-information': UserInformationUserInformation;
     }
   }
