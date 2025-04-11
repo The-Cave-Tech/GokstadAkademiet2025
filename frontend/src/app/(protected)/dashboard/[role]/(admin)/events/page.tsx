ce4b0819cfb96166d62f232a578ea4e48273c034
Simen Skogen <sim1skogen@gmail.com>
2025-04-11 15:02:34 +0200

    Added the page for events. Fixed types to be more in sync with strapi

---
+++ frontend/src/app/(protected)/dashboard/[role]/(admin)/events/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EventsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Events</h1>
+      <p>Welcome to the events page. Manage your events here.</p>
+    </div>
+  );
+};
+
+export default EventsPage;
